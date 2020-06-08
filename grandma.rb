d900a77a60448cc2019fddaad7f7f59aa81141d8
Leonardo Newton <leo253@protonmail.com>
2020-06-08 05:10:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,12 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma(string)
+  if string != string.uppercase
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif string == string.uppercase
+  puts "NO, NOT SINCE 1938!"
+else string == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+end
+  
